05219d9da4b6d43b9568bb7231e2f2d93741895e
ivany2013 <491025640@qq.com>
2018-02-26 16:21:26 +0800

    commit

---
+++ src/main/java/com/example/demo/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Xul on 2018/2/26.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
